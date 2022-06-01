17a1656b5e20d99b074a658383336b0a18449bcd
Ben <60629637+Qzop@users.noreply.github.com>
2022-06-01 11:18:37 -0700

    Add files via upload

--- UHCPlugin/src/core/ConfigVariables/BedRockBorder.java
+++ UHCPlugin/src/core/ConfigVariables/BedRockBorder.java
@@ -7,8 +7,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
 
 public class BedRockBorder implements Listener
 {
@@ -16,92 +19,114 @@ public class BedRockBorder implements Listener
 
     public void setUpBorder()
     {
+        ArrayList<Block> wall = new ArrayList<Block>();
+        World world = Bukkit.getWorld("uhcworld");
+        int z = ConfigInventory.borderSize;
+        int x = ConfigInventory.borderSize;
+
+        boolean posX = false;
+        boolean posZ = false;
+        boolean negX = false;
+        boolean negZ = false;
+
+        while(!posZ)
+        {
+            if(z < -ConfigInventory.borderSize)
+            {
+                posZ = true;
+                z = -1500;
+            }
+            else
+            {
+                for(int y = 0; y < world.getHighestBlockYAt(ConfigInventory.borderSize, z) + 10; y++)
+                {
+                    wall.add(world.getBlockAt(ConfigInventory.borderSize, y, z));
+                }
+
+                world.loadChunk(ConfigInventory.borderSize, z);
+                z--;
+            }
+        }
+
+        while(!negZ && posZ);
+        {
+            if(z > ConfigInventory.borderSize)
+            {
+                negZ = true;
+            }
+            else
+            {
+                for(int y = 0; y < world.getHighestBlockYAt(-ConfigInventory.borderSize, z) + 10; y++)
+                {
+                    wall.add(world.getBlockAt(-ConfigInventory.borderSize, y, z));
+                }
+
+                world.loadChunk(-ConfigInventory.borderSize, z);
+                z++;
+            }
+        }
+
+        while(!posX && posZ && negZ)
+        {
+            if(x < -ConfigInventory.borderSize)
+            {
+                posX = true;
+                x = -1500;
+            }
+            else
+            {
+                for(int y = 0; y < world.getHighestBlockYAt(x, ConfigInventory.borderSize) + 10; y++)
+                {
+                    wall.add(world.getBlockAt(x, y, ConfigInventory.borderSize));
+                }
+
+                world.loadChunk(x, ConfigInventory.borderSize);
+                x--;
+            }
+        }
+
+        while(!negX && posX && posZ && negZ)
+        {
+            if(x > ConfigInventory.borderSize)
+            {
+                negX = true;
+            }
+            else
+            {
+                for(int y = 0; y < world.getHighestBlockYAt(x, -ConfigInventory.borderSize) + 10; y++)
+                {
+                    wall.add(world.getBlockAt(x, y, -ConfigInventory.borderSize));
+                }
+
+                world.loadChunk(x, -ConfigInventory.borderSize);
+                x++;
+            }
+        }
+
+        if(negX && posX && posZ && negZ)
+        {
+            Bukkit.broadcastMessage(ChatColor.GREEN + "All border blocks added to array.");
+        }
+
+
         new BukkitRunnable()
         {
-            World world = Bukkit.getWorld("uhcworld");
-            int z = ConfigInventory.borderSize;
-            int x = ConfigInventory.borderSize;
-
-            boolean posX = false;
-            boolean posZ = false;
-            boolean negX = false;
-            boolean negZ = false;
-
             public void run()
             {
-                if(z < -ConfigInventory.borderSize)
-                {
-                    posZ = true;
-                    z = -1500;
-                }
-                else if(!posZ)
-                {
-                    for(int y = 0; y < world.getHighestBlockYAt(ConfigInventory.borderSize, z) + 10; y++)
-                    {
-                        world.getBlockAt(ConfigInventory.borderSize, y, z).setType(Material.BEDROCK);
-                    }
-
-                    world.loadChunk(ConfigInventory.borderSize, z);
-                    z--;
-                }
-                else if(!negZ && posZ)
-                {
-                    if(z > ConfigInventory.borderSize)
-                    {
-                        negZ = true;
-                    }
-                    else
-                    {
-                        for(int y = 0; y < world.getHighestBlockYAt(-ConfigInventory.borderSize, z) + 10; y++)
-                        {
-                            world.getBlockAt(-ConfigInventory.borderSize, y, z).setType(Material.BEDROCK);
-                        }
-
-                        world.loadChunk(-ConfigInventory.borderSize, z);
-                        z++;
-                    }
-                }
-
-                if(x < -ConfigInventory.borderSize)
-                {
-                    posX = true;
-                    x = -1500;
-                }
-                else if(!posX)
-                {
-                    for(int y = 0; y < world.getHighestBlockYAt(x, ConfigInventory.borderSize) + 10; y++)
-                    {
-                        world.getBlockAt(x, y, ConfigInventory.borderSize).setType(Material.BEDROCK);
-                    }
-
-                    world.loadChunk(x, ConfigInventory.borderSize);
-                    x--;
-                }
-                else if(!negX && posX)
-                {
-                    if(x > ConfigInventory.borderSize)
-                    {
-                        negX = true;
-                    }
-                    else
-                    {
-                        for(int y = 0; y < world.getHighestBlockYAt(x, -ConfigInventory.borderSize) + 10; y++)
-                        {
-                            world.getBlockAt(x, y, -ConfigInventory.borderSize).setType(Material.BEDROCK);
-                        }
-
-                        world.loadChunk(x, -ConfigInventory.borderSize);
-                        x++;
-                    }
-                }
-
-                if(posX && posZ && negX && negZ)
-                {
-                    Bukkit.broadcastMessage(ChatColor.GREEN + "Border finished.");
-                    cancel();
-                }
+               for(int i = 0; i < 2000; i++)
+               {
+                   if(wall.isEmpty())
+                   {
+                       Bukkit.broadcastMessage(ChatColor.GREEN + "Done");
+                       cancel();
+                   }
+                   else
+                   {
+                       wall.get(i).setType(Material.BEDROCK);
+                   }
+               }
             }
 
-        }.runTaskTimer(plugin, 0, 1);
+        }.runTaskTimer(plugin, 0, 40);
     }
 }
