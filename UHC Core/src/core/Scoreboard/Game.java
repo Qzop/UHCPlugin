0390aa034423c4b3850d33f18531209e4862dc94
Fair <60629637+Qzop@users.noreply.github.com>
2022-07-23 17:03:45 -0700

    Delete UHC Core directory

--- UHC Core/src/core/Scoreboard/Game.java
+++
@@ -1,8 +0,0 @@
-package core.Scoreboard;
-
-import org.bukkit.event.Listener;
-
-public class Game implements Listener
-{
-
-}
--- UHC Core/src/core/Scoreboard/Practice.java
+++
@@ -1,8 +0,0 @@
-package core.Scoreboard;
-
-import org.bukkit.event.Listener;
-
-public class Practice implements Listener
-{
-
-}
